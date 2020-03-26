ca89e5b028b7a4b76936ae147660d47ab3c91b16
jakovistuk <jistuk@gmail.com>
2020-03-26 13:25:21 +0100

    check

--- vars/helloWorld.groovy
+++ vars/helloWorld.groovy
@@ -1,5 +1,7 @@
 #!/usr/bin/env groovy
 import com.mpsi.Test2
+import java.util.List
+import java.util.ArrayList
 def call(){
 	println 'HELLO WORLD 1...'
 	//Test2.DisplayName().call()
@@ -7,5 +9,15 @@ def call(){
 	println 'HELLO WORLD 2...'
 	
 	println Test2.test2()
-	
+
+	List<Integer> stringList = new ArrayList<>();
+	stringList.add(1)
+	stringList.add(2)
+	stringList.add(3)
+	stringList.add(4)
+	stringList.add(5)
+
+	println stringList.countBy { it % 2 }
+
+
 }
