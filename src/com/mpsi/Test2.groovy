3923fb814a953ba87c9ea09a882dff142b475f25
jakovistuk <jistuk@gmail.com>
2020-03-26 13:11:12 +0100

    Update Test2.groovy

--- src/com/mpsi/Test2.groovy
+++ src/com/mpsi/Test2.groovy
@@ -4,10 +4,19 @@ class Test2 {
     static def DisplayName() {
         println("XXXXXXXXXXx This is how methods work in groovy");
         println("XXXXXXXXXXy his is an example of a simple method");
+        println("XXXXXXXXXXy his is an example of a simple method");
+        println("XXXXXXXXXXy his is an example of a simple method");
     }
     
     static void test1(){
         println("XXXXXXXXXXx This is how methods work in groovy");
+        println("XXXXXXXXXXy his is an example of a simple method");
+        println("XXXXXXXXXXy his is an example of a simple method");
+        println("XXXXXXXXXXy his is an example of a simple method");
+        println("XXXXXXXXXXy his is an example of a simple method");
+        println("XXXXXXXXXXy his is an example of a simple method");
+        println("XXXXXXXXXXy his is an example of a simple method");
+        println("XXXXXXXXXXy his is an example of a simple method");
         println("XXXXXXXXXXy his is an example of a simple method");
     }
 
